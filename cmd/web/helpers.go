7cef841958ef7ea50f81035e56dafc0248bbcd47
Snir Koppelman <snirkwork@gmail.com>
2022-08-12 16:29:48 +0300

    Add templates and middleware tests

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -10,6 +10,10 @@ import (
 	"github.com/go-playground/form/v4"
 )
 
+func ping(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
+
 func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
